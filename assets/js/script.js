fdc1405cd2a51d3c566ae5fc59118bea8075c151
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-04-26 16:34:03 +0300

    Add task actions with icons and improve task styling
    Integrated Font Awesome for task action icons and enhanced task UI with delete, done, and edit buttons. Refactored JavaScript to dynamically generate task elements and handle actions efficiently. Updated CSS for better styling and hover effects on task action buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 //? create add func
 const input = document.querySelector(".input")
 const addBtn = document.querySelector(".add")
-const tasks = document.querySelector(".tasks")
+const tasksContainer = document.querySelector(".tasks")
 
 // arr to store tasks
 let tasksArr = []
@@ -22,4 +22,45 @@ function addTask(task) {
         completed: false
     }
     tasksArr.push(taskData)
+    addTaskElement(tasksArr)
+}
+
+// add task element
+function addTaskElement(taskArray) {
+    tasksContainer.innerHTML = ""
+
+    tasksArr.forEach((task) => {
+        // create task element
+        const taskElement = document.createElement("div")
+        taskElement.className = "task"
+        taskElement.setAttribute("data-id", task.id)
+        taskElement.appendChild(document.createTextNode(task.text))
+        // create delete button
+        const spanContainer = document.createElement("div")
+        spanContainer.className = "span-container"
+        const deleSpan = document.createElement("span")
+        let deleteIcon = document.createElement("i")
+        deleSpan.className = "delete"
+        deleteIcon.className = "fa-solid fa-trash-can"
+        deleSpan.appendChild(deleteIcon)
+        //create a done button
+        const doneSpan = document.createElement("span")
+        doneSpan.className = "done"
+        let doneIcon = document.createElement("i")
+        doneIcon.className = "fa-solid fa-check"
+        doneSpan.appendChild(doneIcon)
+        // create edit button
+        const editSpan = document.createElement("span")
+        editSpan.className = "edit"
+        let editIcon = document.createElement("i")
+        editIcon.className = "fa-solid fa-pen-to-square"
+        editSpan.appendChild(editIcon)
+        // append children
+        spanContainer.appendChild(doneSpan)
+        spanContainer.appendChild(editSpan)
+        spanContainer.appendChild(deleSpan)
+        taskElement.appendChild(spanContainer)
+        tasksContainer.appendChild(taskElement)
+    })
+
 }
