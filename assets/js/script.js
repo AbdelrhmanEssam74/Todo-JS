ea1c50bb96109405ed51095329f4311878b5e646
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-04-28 01:47:50 +0300

    create srote in localstorage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,44 +23,53 @@ function addTask(task) {
     }
     tasksArr.push(taskData)
     addTaskElement(tasksArr)
+    storeTasks()
 }
 
 // add task element
-function addTaskElement(taskArray) {
+function addTaskElement(Array) {
     tasksContainer.innerHTML = ""
-
-    tasksArr.forEach((task) => {
-        // create task element
-        const taskElement = document.createElement("div")
-        taskElement.className = "task"
-        taskElement.setAttribute("data-id", task.id)
-        taskElement.appendChild(document.createTextNode(task.text))
-        // create delete button
-        const spanContainer = document.createElement("div")
-        spanContainer.className = "span-container"
-        const deleSpan = document.createElement("span")
-        let deleteIcon = document.createElement("i")
-        deleSpan.className = "delete"
-        deleteIcon.className = "fa-solid fa-trash-can"
-        deleSpan.appendChild(deleteIcon)
-        //create a done button
-        const doneSpan = document.createElement("span")
-        doneSpan.className = "done"
-        let doneIcon = document.createElement("i")
-        doneIcon.className = "fa-solid fa-check"
-        doneSpan.appendChild(doneIcon)
-        // create edit button
-        const editSpan = document.createElement("span")
-        editSpan.className = "edit"
-        let editIcon = document.createElement("i")
-        editIcon.className = "fa-solid fa-pen-to-square"
-        editSpan.appendChild(editIcon)
-        // append children
-        spanContainer.appendChild(doneSpan)
-        spanContainer.appendChild(editSpan)
-        spanContainer.appendChild(deleSpan)
-        taskElement.appendChild(spanContainer)
-        tasksContainer.appendChild(taskElement)
+    Array.forEach((task) => {
+        createTaskElement(task)
     })
 
+}
+// create task element
+function createTaskElement(task) {
+    // create task element
+    const taskElement = document.createElement("div")
+    taskElement.className = "task"
+    taskElement.setAttribute("data-id", task.id)
+    taskElement.appendChild(document.createTextNode(task.text))
+    // create delete button
+    const spanContainer = document.createElement("div")
+    spanContainer.className = "span-container"
+    const deleSpan = document.createElement("span")
+    let deleteIcon = document.createElement("i")
+    deleSpan.className = "delete"
+    deleteIcon.className = "fa-solid fa-trash-can"
+    deleSpan.appendChild(deleteIcon)
+    //create a done button
+    const doneSpan = document.createElement("span")
+    doneSpan.className = "done"
+    let doneIcon = document.createElement("i")
+    doneIcon.className = "fa-solid fa-check"
+    doneSpan.appendChild(doneIcon)
+    // create edit button
+    const editSpan = document.createElement("span")
+    editSpan.className = "edit"
+    let editIcon = document.createElement("i")
+    editIcon.className = "fa-solid fa-pen-to-square"
+    editSpan.appendChild(editIcon)
+    // append children
+    spanContainer.appendChild(doneSpan)
+    spanContainer.appendChild(editSpan)
+    spanContainer.appendChild(deleSpan)
+    taskElement.appendChild(spanContainer)
+    tasksContainer.appendChild(taskElement)
+}
+
+// store tasks in local storage
+function storeTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasksArr))
 }
